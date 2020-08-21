bf883a2ea8eb525815c864a310e156903919041e
relykstm <relykstm@gmail.com>
2020-08-21 12:27:40 -0700

    Friday

--- server.py
+++ server.py
@@ -3,19 +3,25 @@ import re
 app = Flask(__name__)
 
 
-SQL_INJECTION_REGEX_1 = re.compile(r'(=)|(<)|(>)|(\')|(--)|(\/)|(\+)|(;)|(\*)|(!)|({)|(})|(drop table)|(drop stored)|(alter table)|(alter stored)|(sp_)|(xp_)|(exec )|(execute )|(fetch)|(select)|(kill)|(selectsys)|(sysobjects)|(syscolumns)|(isnull)|(coalesce)|(dbo)|(tbl)|(usp)')
+SQL_INJECTION_REGEX_1 = re.compile(r'(&lt;)|(&gt;)|(%3c)|(%3e)|(=)|(<)|(>)|(\')|(--)|(\/)|(\+)|(;)|(\*)|(!)|({)|(})|(drop table)|(drop stored)|(alter table)|(alter stored)|(sp_)|(xp_)|(exec )|(execute )|(fetch)|(select)|(kill)|(selectsys)|(sysobjects)|(syscolumns)|(isnull)|(coalesce)|(dbo)|(tbl)|(usp)|(&amp;lt;)|(&amp;gt;)')
       
 @app.route('/v1/sanitized/input/', methods=['POST'])
 
 def test_input():
     response = {}
-    
+    #input json format: {'payload': "value"}
     #assigns payload value to variable called input
 
     input = request.json
-    print("input:  ",input ['payload'])
+    #if JSON is not format {'payload': "value"}, reponse will return error message:
+    try:
+        print("input:  ",input ['payload'])
+    except:
+        return {'result' : "ERROR: Incorrect Data Format"}
+    
 
-    # if any match is found respond with {'result': 'unsanitized'}.  If no matches found respond with {'result': 'sanitized'}
+    # if any match is found respond with {'result': 'unsanitized'}.  
+    # If no matches found respond with {'result': 'sanitized'}
 
     if SQL_INJECTION_REGEX_1.search(input ['payload']):
         response ['result'] = 'unsanitized'
--- test_sql_injection.py
+++ test_sql_injection.py
@@ -1,59 +1,84 @@
-import tempfile, requests, pytest, json
+import requests, pytest, json
+
+url = "http://localhost:5000/v1/sanitized/input/"
 
 def test_post_api_1():
-    url = "http://localhost:5000/v1/sanitized/input/"
     response = requests.request("POST", url, json = {'payload': 'hello'})
     res = json.loads(response.text)
     print("Test 1: Input = hello, should respond with sanitized.  Actually responded with: " + res['result'])
     assert res['result'] == "sanitized"
     
 def test_post_api_2():
-    url = "http://localhost:5000/v1/sanitized/input/"
-    payload = {}
     response = requests.request("POST", url,json = {'payload': '--'})
     res = json.loads(response.text)
     print("Test 2: Input = -- , should respond with unsanitized.  Actually responded with: " + res['result'])
     assert res['result'] == "unsanitized"
 
-
 def test_post_api_3():
-    url = "http://localhost:5000/v1/sanitized/input/"
     response = requests.request("POST", url,json = {'payload': '1\'or\'1\'=\'1'})
     res = json.loads(response.text)
     print("Test 3: Input = '1'='1 , should respond with unsanitized.  Actually responded with: " + res['result'])
     assert res['result'] == "unsanitized"
 
 def test_post_api_4():
-    url = "http://localhost:5000/v1/sanitized/input/"
-    payload = {}
     response = requests.request("POST", url,json = {'payload': '1\'or\'1\'=\'1'})
     res = json.loads(response.text)
     print("Test 4: Input = 1'or2>1--, should respond with unsanitized.  Actually responded with: " + res['result'])
     assert res['result'] == "unsanitized"
 
 def test_post_api_5():
-    url = "http://localhost:5000/v1/sanitized/input/"
-    payload = {}
     response = requests.request("POST", url,json = {'payload': '105 OR 1=1'})
     res = json.loads(response.text)
     print("Test 5: Input = 105 OR 1=1, should respond with unsanitized.  Actually responded with: " + res['result'])
     assert res['result'] == "unsanitized"
 
 def test_post_api_6():
-    url = "http://localhost:5000/v1/sanitized/input/"
-    payload = {}
     response = requests.request("POST", url,json = {'payload': '\" or \"\"=\"\"'})
     res = json.loads(response.text)
     print("Test 6: Input = \" or \"\"=\"\", should respond with unsanitized.  Actually responded with: " + res['result'])
     assert res['result'] == "unsanitized"
 
 def test_post_api_7():
-    url = "http://localhost:5000/v1/sanitized/input/"
-    payload = {}
     response = requests.request("POST", url,json = {'payload': '105; DROP TABLE Suppliers'})
     res = json.loads(response.text)
-    print("Test 6: Input = 105; DROP TABLE Suppliers, should respond with unsanitized.  Actually responded with: " + res['result'])
+    print("Test 7: Input = 105; DROP TABLE Suppliers, should respond with unsanitized.  Actually responded with: " + res['result'])
     assert res['result'] == "unsanitized"
+
+def test_post_api_8():
+    response = requests.request("POST", url,json = {'payload': '%3e'})
+    res = json.loads(response.text)
+    print("Test 8: Input = '%3e', should respond with unsanitized.  Actually responded with: " + res['result'])
+    assert res['result'] == "unsanitized"
+
+def test_post_api_9():
+    response = requests.request("POST", url,json = "string")
+    res = json.loads(response.text)
+    print("Test 9: Input = string, not correct json format, should respond with ERROR: Incorrect Data Format.  Actually responded with: " + res['result'])
+    assert res['result'] == "ERROR: Incorrect Data Format"
+
+def test_post_api_10():
+    response = requests.request("POST", url, json = [1,2,3])
+    res = json.loads(response.text)
+    print("Test 10: Input = List[Int], not correct json format, should respond with ERROR: Incorrect Data Format.  Actually responded with: " + res['result'])
+    assert res['result'] == "ERROR: Incorrect Data Format"
+
+def test_post_api_11():
+    response = requests.request("POST", url, json = {'payload': '08/21/2020'})
+    res = json.loads(response.text)
+    print("Test 11: Input = 08/21/2020, should respond with sanitized.  Actually responded with: " + res['result'])
+    assert res['result'] == "sanitized"
+
+def test_post_api_12():
+    response = requests.request("POST", url, json = {'payload': '08-21-2020'})
+    res = json.loads(response.text)
+    print("Test 12: Input = 08-21-2020, should respond with sanitized.  Actually responded with: " + res['result'])
+    assert res['result'] == "sanitized"
+
+def test_post_api_13():
+    response = requests.request("POST", url, json = {'payload': 'relykstm@gmail.com'})
+    res = json.loads(response.text)
+    print("Test 13: Input = relykstm@gmail.com, should respond with sanitized.  Actually responded with: " + res['result'])
+    assert res['result'] == "sanitized"
 
 
 
